482909b0892abec840815c5243a2d97d993b4a8d
Gopi11690 <gopibox@gmail.com>
2024-11-21 17:13:34 +0530

    index.html small update

--- script.js
+++ script.js
@@ -1,37 +1,42 @@
-let customerName="Barath";
-let purchaseAmt=3550;
+//BigBasket Reward Point to Gift Voucher System
 
-let voucherValue=0;
+
+// Declaring Variables
+
+let customerName="Barath";                           // You can change Customer Name
+let purchaseAmt=5000;                               // You can change Purchase Amount
+
+
+//Function for calculating Bonus Points
 
 function calculateBonusPoint(amt){
-    
     let bonus=0;
-   
+
     if(amt>=100 && amt<1000){
         bonus=0.05;
-    }
-    else if(amt>=1000 && amt<2500){
+    }else if(amt>=1000 && amt<2500){
         bonus=0.1;
     }else if(amt>=2500 && amt<5000){
         bonus=0.15;
-    }
-    else{
+    }else{
         bonus=0.2;
-    }
-    return amt*bonus;
+    }return amt*bonus;                                   //Return the Bonus Points Value
 }
 
-let bonusPoints= calculateBonusPoint(purchaseAmt);
-if(purchaseAmt<100){
-    console.log("Thank You For Visiting Us!!!");
+let bonusPoints= calculateBonusPoint(purchaseAmt);      // Storing Bonus Point into variable
+
+if(purchaseAmt<100){                                    // For Purchase value less than 100 
+    console.log("Thank You For Visiting Us!!!");     
 }
 else{
-    voucherValue=Math.round(bonusPoints);
+    let voucherValue=Math.round(bonusPoints);           //Round Value for Voucher Calculation
     
     console.log(`Welcome ${customerName}!`);
     console.log(`Your Purchase Amount Rs. ${purchaseAmt}.\n You have rewarded Rs.${voucherValue} Gift Voucher for your ${bonusPoints} Bonus Points...`);
-    let optionNum=parseInt(prompt("You can Select Reward Points from Bonus Points by choosing below Options : \n1 to redeem all \n2 to redeem 50%  \n0 to Cancel"));
-    switch(optionNum){
+
+    let optionNum=parseInt(prompt("You can Select Reward Points from Bonus Points by choosing below Options : \n1 to redeem all \n2 to redeem 50%  \n0 to Cancel"));    //Showing Prompt to select the Redeem Option
+    
+    switch(optionNum){                                   // SwitchCase for Redeem Options
         case 0:
             console.log(`You cancelled the redeem option \nYou have pay Rs. ${purchaseAmt} \nYou ${voucherValue} remaining Gift Voucher`);
             break;
